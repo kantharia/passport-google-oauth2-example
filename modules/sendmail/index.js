abed7c38cc19b1960513712e4554de61b8638e03
Chetan Kantharia <chetan.kantharia@gmail.com>
2016-11-24 15:10:00 +0530

    Using Amazon SES for SMTP

--- modules/sendmail/index.js
+++ modules/sendmail/index.js
@@ -1,27 +1,35 @@
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('chetan.kantharia@gmail.com');
+var ses = require('node-ses');
+var client = ses.createClient({
+  key: process.env.AWS_SES_KEY,
+  secret: process.env.AWS_SES_SECRET,
+  amazon:process.env.AWS_SES_SERVER
+});
+
+console.log('AWS Server : ', process.env.AWS_SES_SERVER);
+
+var from_email = 'chetan.kantharia@gmail.com';
 var subject = 'Intro.ooo - Account Activation';
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
 
 module.exports = {
   sendOTPEmail: function(data, meta_data){
     var mailContent = "Please active your account by entering OTP:" + data.otp;
-        mailContent += "\n\n Visit : " + meta_data.app_host +"/activation to activate your account."
-    var content = new helper.Content('text/plain', mailContent)
-    var to_email = new helper.Email(data.username);
-    var mail = new helper.Mail(from_email, subject, to_email, content);
+        mailContent += "<br/><br/>Visit : " + meta_data.app_host +"/activation to activate your account."
+    var to_email = data.username;
 
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON(),
-    });
-
-    sg.API(request, function(error, response) {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
-
+    client.sendEmail({
+        to: to_email,
+        from: from_email,
+        subject: subject,
+        message: mailContent
+      },
+      function(err, data, res){
+        if(err){
+          console.log('Sending OTP Error:', err);
+        }
+        if(res){
+          console.log('Activation mail sent to:', to_email);
+        }
+     })
   }
 }
