7d98d150f7ceff6a36ddcbb52b356a2fcb89cc33
Chetan Kantharia <chetan.kantharia@gmail.com>
2016-11-25 13:26:06 +0530

    Adding lib.js

---
+++ public/src/lib.js
@@ -0,0 +1,50 @@
+console.log('Some libs');
+
+function isEmailValid(email){
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+var app = {
+  signupUser: function(){
+    console.log('Signup User');
+
+    //Get username and password
+    var username = document.getElementById('username');
+        username = username ? username.value : undefined;
+
+    var password = document.getElementById('password');
+        password = password ? password.value : undefined;
+
+    console.log('Got username and password', username, password);
+
+    if(!isEmailValid(username)){
+      document.getElementById("status").innerHTML = "Email is invalid";
+    }
+
+    if(username && password && isEmailValid(username)){
+      //submit form
+      axios.post('/user', {username:username, password:password})
+        .then(function(response){
+          var _html = "<b>User Created successfully<b>"
+              _html += "<p>We have sent you OTP. Please check you email and activte your account</p>";
+          document.getElementById("signupform").innerHTML = _html;
+        })
+        .catch(function(error){
+          if(error && error.response){
+            alert("Error:" + error.response.data.status);
+          } else {
+            alert("Something went wrong please try again later.");
+          }
+        })
+
+    } else {
+
+      if(!isEmailValid(username)) {
+        alert("Invalid Email Address");
+      } else {
+        //show error
+        alert("Username or password is blank");
+      }
+    }
+  }
+}
